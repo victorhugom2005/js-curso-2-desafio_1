47d64ccb00da41871ce1a6ca26f1b5e431546924
victorhugom2005 <victorhugomarcondes2006@gmail.com>
2025-08-29 12:20:57 -0300

    melhorando o código

--- app.js
+++ app.js
@@ -16,7 +16,24 @@ function chuteCidade() {
 
 function chuteMath() {
     let numero1 = parseInt(prompt('Digite o primeiro número'))
-    let numero2 = parseInt(prompt('Digite o segundo número'))
+    let numero2 = Number(prompt('Digite o segundo número'))
     let resultado = numero1 + numero2;
-    alert(`${numero1} + ${numero2} = ${resultado}`)
+    if (Number.isNaN(resultado)){
+        alert('Resultado Inválido')
+        chuteMath()
+    } else {
+        alert(`${numero1} + ${numero2} = ${resultado}`)
+    }
+}
+
+function AreaTri() {
+    let base = parseInt(prompt('Digite a base do triângulo'))
+    let altura = Number(prompt('Digite a altura do triângulo'))
+    let resultado = base * altura / 2;
+    if (Number.isNaN(resultado)){
+        alert('Resultado Inválido')
+        AreaTri()
+    } else {
+        alert(`A área do triângulo é ${resultado}`)
+    }
 }
